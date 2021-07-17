5cd1f59bf26ac6c49dca72edc5fcecfa660f217f
Fernanda Carvalho <fe97964@gmail.com>
2021-07-17 19:24:08 -0300

    Command: Stop - Done

---
+++ commands/music/stop.js
@@ -0,0 +1,17 @@
+const execute = (bot, message, args) => {
+    const queue = bot.queues.get(message.guild.id);
+
+    if(!queue){
+        return message.reply("There is no music playing!")
+    } else {
+        queue.songs = []
+        bot.queues.set(message.guild.id, queue)
+        queue.dispatcher.end()
+    }
+}
+
+module.exports = {
+    name: "stop",
+    help: "Stop playing music || >stop",
+    execute,
+}
