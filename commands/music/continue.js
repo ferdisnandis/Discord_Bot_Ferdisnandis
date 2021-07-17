6b68cb6b71c28b645979465c40b394aa49d75c31
Fernanda Carvalho <fe97964@gmail.com>
2021-07-17 19:12:32 -0300

    Command: Continue - Done

---
+++ commands/music/continue.js
@@ -0,0 +1,15 @@
+const execute = (bot, message, args) => {
+    const queue = bot.queues.get(message.guild.id);
+
+    if(!queue){
+        return message.reply("There is no music playing!")
+    } else {
+        queue.dispatcher.resume()
+    }
+}
+
+module.exports = {
+    name: "continue",
+    help: "Keep playing music after pause || >continue",
+    execute,
+}
