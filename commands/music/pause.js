9994d2cb94800c7eff94f18df7125e85de95cf1a
Fernanda Carvalho <fe97964@gmail.com>
2021-07-17 19:08:03 -0300

    Command: Pause - Done!

---
+++ commands/music/pause.js
@@ -0,0 +1,15 @@
+const execute = (bot, message, args) => {
+    const queue = bot.queues.get(message.guild.id);
+
+    if(!queue){
+        return message.reply("There is no music playing!")
+    } else {
+        queue.dispatcher.pause()
+    }
+}
+
+module.exports = {
+    name: "pause",
+    help: "Pause the current song || >pause",
+    execute,
+}
