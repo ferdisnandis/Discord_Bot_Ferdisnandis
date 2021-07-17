a99a37b57856bdd52f88eeab18c45b9ff16b5eae
Fernanda Carvalho <fe97964@gmail.com>
2021-07-17 19:21:20 -0300

    Command: Volume - Done

---
+++ commands/music/volume.js
@@ -0,0 +1,22 @@
+const execute = (bot, message, args) => {
+    const queue = bot.queues.get(message.guild.id);
+
+    if(!queue){
+        return message.reply("There is no music playing!");
+    }
+
+    const volume = Number(args.join(" "))
+    if(isNaN(volume) || volume < 0 || volume > 10){
+        return message.reply("The volume must be between 1 and 10!") 
+    }
+
+    queue.dispatcher.setVolume(volume/10)
+    queue.volume = volume;
+    bot.queues.set(message.guild.id, queue)
+}
+
+module.exports = {
+    name: "volume",
+    help: "Change the music volume || >volume [number]",
+    execute
+}
